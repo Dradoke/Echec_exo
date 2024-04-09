6ed42d3b526630ef366c6855071de58290453912
Dradoke <nathan.gaudouin76190@gmail.com>
2024-04-09 14:37:23 +0200

    Debut PieceEchecs

---
+++ entities/PieceEchecs.php
@@ -0,0 +1,79 @@
+<?php
+
+class PieceEchecs
+{
+
+    private $x;
+    private $y;
+    private $couleur;
+
+
+    public function __construct(int $x, int $y, Couleurs $couleur){
+
+        $this->setX($x);
+        $this->setY($y);
+        
+        
+    }
+
+
+    /**
+     * Get the value of couleur
+     */ 
+    public function getCouleur()
+    {
+        return $this->couleur;
+    }
+
+    /**
+     * Set the value of couleur
+     *
+     * @return  self
+     */ 
+    public function setCouleur($couleur)
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of y
+     */ 
+    public function getY()
+    {
+        return $this->y;
+    }
+
+    /**
+     * Set the value of y
+     *
+     * @return  self
+     */ 
+    public function setY($y)
+    {
+        $this->y = $y;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of x
+     */ 
+    public function getX()
+    {
+        return $this->x;
+    }
+
+    /**
+     * Set the value of x
+     *
+     * @return  self
+     */ 
+    public function setX($x)
+    {
+        $this->x = $x;
+
+        return $this;
+    }
+}
